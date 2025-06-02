ac01b2b6929f65f9c59f29d1781d5064237a5f47
Arpit Biyani <arpitbiyaniaz@gmail.com>
2025-06-02 10:47:17 +0000

    DONT USE BLACK BACKGROUND USE GRADENDES IN IT

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen bg-background">
+    <div className="flex flex-col min-h-screen"> {/* Removed bg-background */}
       <AppHeader />
       <main className="container mx-auto px-4 py-8 flex-grow">
         <section className="text-center mb-10 md:mb-16">
